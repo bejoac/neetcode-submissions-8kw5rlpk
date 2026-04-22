b0080206e1a54314426645fa3daa82d031ebbb9c
bejoac <69582270+bejoac@users.noreply.github.com>
2026-04-22 15:44:28 +0200

    Add: network-delay-time - submission-7

---
+++ Data Structures & Algorithms/network-delay-time/submission-7.py
@@ -0,0 +1,26 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        adjacency_map = defaultdict(list)
+        time_to_node = defaultdict(lambda: float("inf"))
+        time_to_node[k] = 0
+
+        for src, dst, time in times:
+            adjacency_map[src].append((time, dst))
+
+        heap = [(0, k)]
+
+        while heap:
+            time, node = heapq.heappop(heap)
+
+            if time > time_to_node[node]:
+                continue
+
+            for neighbor_time, neighbor_node in adjacency_map[node]:
+                if neighbor_time + time < time_to_node[neighbor_node]:
+                    time_to_node[neighbor_node] = neighbor_time + time
+                    heapq.heappush(heap, (time + neighbor_time, neighbor_node))
+
+        if len(time_to_node) == n:
+            return max(time_to_node.values())
+        else:
+            return -1
