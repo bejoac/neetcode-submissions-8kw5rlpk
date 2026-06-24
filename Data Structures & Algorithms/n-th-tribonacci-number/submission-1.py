5d295dd283bb48fa4d20ca04f0c3e4f4cd780f76
bejoac <69582270+bejoac@users.noreply.github.com>
2026-06-24 15:38:24 +0200

    Add: n-th-tribonacci-number - submission-1

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        memo = {0: 0, 1: 1, 2: 1}
+
+        for i in range(n + 1):
+            if i < 3:
+                continue
+            else:
+                result = memo[i-1] + memo[i-2] + memo[i-3]
+                memo[i] = result
+
+        return memo[n]
