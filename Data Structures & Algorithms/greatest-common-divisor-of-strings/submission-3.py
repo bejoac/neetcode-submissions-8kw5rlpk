5faffb7f783e4a598c94b1c20a1c6c48d0845a8d
bejoac <69582270+bejoac@users.noreply.github.com>
2026-06-24 12:51:18 +0200

    Add: greatest-common-divisor-of-strings - submission-3

---
+++ Data Structures & Algorithms/greatest-common-divisor-of-strings/submission-3.py
@@ -0,0 +1,43 @@
+class Solution:
+    def check_divident(self, divident: str, divisor: str) -> bool:
+        multiple_divisor = divisor
+
+        while len(multiple_divisor) <= len(divident):
+            if multiple_divisor == divident:
+                return True
+            multiple_divisor += divisor
+
+        return False
+
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        chars1 = []
+
+        for char in str1:
+            if char not in chars1:
+                chars1.append(char)
+
+        chars2 = []
+
+        for char in str2:
+            if char not in chars2:
+                chars2.append(char)
+
+        if chars1 != chars2:
+            return ""
+
+        smaller = min(str1, str2, key=len)
+        bigger = max(str1, str2, key=len)
+        
+        while len(smaller) >= 0:
+            if self.check_divident(bigger, smaller):
+                return smaller
+
+            old_smaller = smaller
+            smaller = smaller[:-1]
+
+            while len(smaller) > 0:
+                if self.check_divident(old_smaller, smaller):
+                    break
+                smaller = smaller[:-1]
+
+        return ""
